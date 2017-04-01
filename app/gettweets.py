90f1613a6d8f5a94ead56e2c018e10829ad8e371
Megan Weijiang <meganweijiang@utexas.edu>
2017-04-01 15:52:21 -0500

    template for results page and fix gettweets.py

--- app/gettweets.py
+++ app/gettweets.py
@@ -1,15 +1,15 @@
 import json
 import tweepy
 
-API_KEY = 'ypOZYLGlzL83c8RcG0vkUIJ6Y'
-API_SECRET = '60V0stdGQf6KYveJ6jpb33y9lFAw2sHYuGa9IZvSCjm70cciSH'
-TOKEN_KEY = '803397824822083584-0J6i6wpp6KdGqZPIexlAVdGs6MhPNCp'
-TOKEN_SECRET = 'twwHBypmQrnFPS0JiS3tHvNJUJ5px1LEvgKooYBaHNm9F'
+#API_KEY = 'ypOZYLGlzL83c8RcG0vkUIJ6Y'
+#API_SECRET = '60V0stdGQf6KYveJ6jpb33y9lFAw2sHYuGa9IZvSCjm70cciSH'
+#TOKEN_KEY = '803397824822083584-0J6i6wpp6KdGqZPIexlAVdGs6MhPNCp'
+#TOKEN_SECRET = 'twwHBypmQrnFPS0JiS3tHvNJUJ5px1LEvgKooYBaHNm9F'
 
-#API_KEY = 'HJIj9KVlhARSZFwG7CCSjqwOS'
-#API_SECRET = 'JdfdIVeSNLFuazbzSZeKAofdy7s5IzLVpI25RgjOzOuOreONtT'
-#TOKEN_KEY = '3243742999-ItaCs47Neu3ve4dTJpcsVz4xvYuJxBSYbrzTRa0'
-#TOKEN_SECRET = 'mArLXKMpbWArgRRI8UQqYrbJWbZyQxTXmgaMeh9J19CEm'
+API_KEY = 'HJIj9KVlhARSZFwG7CCSjqwOS'
+API_SECRET = 'JdfdIVeSNLFuazbzSZeKAofdy7s5IzLVpI25RgjOzOuOreONtT'
+TOKEN_KEY = '3243742999-ItaCs47Neu3ve4dTJpcsVz4xvYuJxBSYbrzTRa0'
+TOKEN_SECRET = 'mArLXKMpbWArgRRI8UQqYrbJWbZyQxTXmgaMeh9J19CEm'
 
 jsonList = []
 
@@ -26,10 +26,13 @@ def getData(api_inst, twitter_query):
 	counter = 0
 	twitter_cursor = tweepy.Cursor(api_inst.search, q=twitter_query, lang="en")
 	for page in twitter_cursor.pages():
+		if counter == 50:
+			break
 		for item in page:
-			if counter == 10:
+			if counter == 50:
 				break
 			else:
 				store(item.text)
 				counter += 1
 				print("Found a tweet")
+	return
