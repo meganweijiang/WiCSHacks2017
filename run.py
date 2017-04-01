75441f51c5e05fd62c5c588d27a00d0d974a4cc8
Tiffany Tso <thetiffanytso@gmail.com>
2017-04-01 13:24:29 -0500

    added index.html and static CSS and JS folders

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 #!flask/bin/python
-from app import app
-app.run(debug=True)
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    author = "Megan and Tiffany"
+    name = "WiCS Hacks 2017"
+    return render_template('index.html', author=author, name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
